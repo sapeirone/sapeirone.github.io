0e5d531820f67bbecb110182a15ff991354792de
sapeirone <16292907+sapeirone@users.noreply.github.com>
2025-05-31 19:54:09 +0000

    Deploying to gh-pages from @ sapeirone/sapeirone.github.io@2a582571f1eff4dc4f71eae6ca610a547f210aaa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,12 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-our-journal-article-egocentric-zone-aware-action-recognition-across-environments-was-accepted-at-pattern-recognition-letters",
+        },{id: "news-our-journal-article-egocentric-zone-aware-action-recognition-across-environments-was-accepted-at-pattern-recognition-letters",
           title: '🚀 Our journal article “Egocentric zone-aware action recognition across environments” was accepted at...',
           description: "",
           section: "News",},{id: "news-now-visiting-the-lts4-lab-at-epfl-under-the-supervision-of-prof-pascal-frossard-working-at-the-intersection-between-concepts-learning-and-video-understanding",
@@ -47,101 +42,7 @@ ninja.data = [{
           section: "News",},{id: "news-we-are-presenting-hiero-and-hier-egopack-at-the-egovis-workshop-and-at-the-workshop-on-visual-concepts-cvpr-2025-see-you-there",
           title: '📢 We are presenting HiERO and Hier-EgoPack at the EgoVis Workshop and at...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
-        id: 'social-bluesky',
-        title: 'Bluesky',
-        section: 'Socials',
-        handler: () => {
-          window.open("simonepeirone.bsky.social", "_blank");
-        },
-      },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%73%69%6D%6F%6E%65.%70%65%69%72%6F%6E%65@%70%6F%6C%69%74%6F.%69%74", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/sapeirone", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/simone-peirone", "_blank");
-        },
-      },{
-        id: 'social-orcid',
-        title: 'ORCID',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://orcid.org/0009-0007-3204-3207", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=K0efPssAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-semanticscholar',
-        title: 'Semantic Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.semanticscholar.org/author/2283647503", "_blank");
-        },
-      },{
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
